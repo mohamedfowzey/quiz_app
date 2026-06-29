1709e71cbfa2782399413522ee19388abb6cc34c
MohamedFowzey <fowzeym@gmail.com>
2026-06-29 23:16:11 +0300

    fix error final

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,13 +1,12 @@
 import CustomInput from "@/app/(components)/auth_components/custom_input/CustomInput";
-import { Input } from "@/components/ui/input";
 import { Mail } from "lucide-react";
 
 export default function Register() {
   return (
     <>
       <h1>Register </h1>
-      <Input type="email" />
-      {/* <CustomInput type="email" StartIcon={Mail}/> */}
+      {/* <Input type="email" /> */}
+      <CustomInput type="email" StartIcon={Mail}/>
     </>
   );
 }
