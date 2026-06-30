93d3dbd3ea70879e7c75e4f5fb04ff839036f7a9
Abdelrahman Mohamed <abdelrhmanaboyehia@gmail.com>
2026-06-30 13:46:39 +0300

    login Page & AuthTabs Component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,31 @@
-export default function Login(){
-    return <h1>login page</h1>
+import AuthTabs from "@/app/(components)/auth_components/AuthTabs/AuthTabs";
+import CustomButton from "@/app/(components)/auth_components/custom_button_auth/CustomButton";
+import CustomInput from "@/app/(components)/auth_components/custom_input/CustomInput";
+import { Mail } from "lucide-react";
+
+export default function Login() {
+  return (
+    <>
+      <AuthTabs />
+      <form action="">
+        <CustomInput
+          className="my-2"
+          placeHolder={"Type your email"}
+          type="email"
+          StartIcon={Mail}
+          labelText={"Registered email address"}
+        />
+
+        <CustomInput
+          className="my-2"
+          placeHolder={"Type your password"}
+          type="password"
+          StartIcon={Mail}
+          labelText={"Password"}
+        />
+
+        <CustomButton headerText={" Sign In "} />
+      </form>
+    </>
+  );
 }
