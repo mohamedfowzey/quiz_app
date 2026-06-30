d40040d27b4c017780b855c5abd5f75df3a616ed
Antonios Farhood <tonefarhoud3433@gmail.com>
2026-06-30 14:21:31 +0300

    change password form ui

--- app/(auth)/change_password/page.tsx
+++ app/(auth)/change_password/page.tsx
@@ -1,3 +1,31 @@
-export default function ChangePassword(){
-    return <h1>change password</h1>
+import CustomButton from "@/app/(components)/auth_components/custom_button_auth/CustomButton";
+import CustomInput from "@/app/(components)/auth_components/custom_input/CustomInput";
+import { LucideKeySquare } from "lucide-react";
+
+export default function ChangePassword() {
+  return (
+    <>
+      <h2 className="text-Header mb-10 "> Change password</h2>
+
+      <form action="">
+        <CustomInput
+          className="my-2"
+          placeHolder={"Type your old password"}
+          type="password"
+          StartIcon={LucideKeySquare}
+          labelText={"Old Password"}
+        />
+
+        <CustomInput
+          className="my-2"
+          placeHolder={"Type your new password"}
+          type="password"
+          StartIcon={LucideKeySquare}
+          labelText={"New Password"}
+        />
+
+        <CustomButton headerText={" Change "} />
+      </form>
+    </>
+  );
 }
